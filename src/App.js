1f2a9aa8f154be226cb50cbe88fa916bf74d4a66
Alex Savchuk <alexsavchukjr@gmail.com>
2019-05-28 19:28:57 +1200

    added button to add a task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Paper, List} from '@material-ui/core';
+import {Paper, List, Button} from '@material-ui/core';
 import {Delete} from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
 import Task from './Task'
@@ -7,32 +7,32 @@ import Task from './Task'
 const styles = {
   card: {
     width: '100%'
+  },
+  button:{
+    backgroundColor:"green"
   }
 }
 
-const mockTasks = [{
+const sampleTask = {
   title:"Lorem Ipsum",
   text:"kjhrskjfhg fjghfjg fjdsgtg nnfruufh  dy..",
   isComplete: false,
   isArchived: false,
   dateCreated: new Date(),
   dateUpdated: new Date()
-},
-{
-  title:"Lorem Ipsum2.0",
-  text:"kjhgksjnhb khsgfiuhf jhsnfkjgh   uidsfyt.!",
-  isComplete: false,
-  isArchived: false,
-  dateCreated: new Date(),
-  dateUpdated: new Date()
-}]
+}
+
 const App = ({classes}) => {
-  const [tasks,addTask] = useState(mockTasks)
+  const [tasks,addTask] = useState([])
   return (
     <Paper>
       <List>
         {tasks.map(task => <Task state={task}/>)}
       </List>
+      <Button onClick={() => addTask(prevTasks => [...prevTasks, sampleTask])}
+      className={classes.button}>
+        New Task
+      </Button>  
     </Paper>
   )
 }
